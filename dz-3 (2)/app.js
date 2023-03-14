450d9d7f878c0c03bc25476c53582373ef0dcbe1
BakytbekovTalgat <127601643+BakytbekovTalgat@users.noreply.github.com>
2023-03-14 23:49:34 +0600

    Сделал дз-3

---
+++ dz-3 (2)/app.js
@@ -0,0 +1,54 @@
+// console.log('Me name is Talgat')
+
+// console.log(20 * 2)
+// console.log(20 / 2)
+// console.log(20 + 2)
+// console.log(20 - 2)
+
+//  Переменные - (Англ- veriable)
+
+// var name = 'Talgat'
+// console.log(name)
+
+// Типы Данных - Т-д всего 8 штук (но 2 типа данных не признаются)
+// object - не приметивный дааный
+
+// var string = 'Hello'  // Тип данных (string)
+
+// var number = 123456 = // Тип данных (number)
+
+// var boolean1 = false // boolean
+// var boolean2 = true // boolean
+
+// var neorp = undefined // undefined
+
+// var nichego = null // null
+
+// var nan = 12 - 'два' // NaN - not a nomber
+ 
+
+// typeof - оператор для вычисления типов данных у переменных
+
+// console.log(typeof nichego)
+// Array - Массив
+// var arr = [12, 'str', null, [string], false]
+// console.log(arr[3][0]);
+
+// var array2 = [1, 2, 3, 4]
+// console.log(array2[2]);
+// Object - объект
+
+// var arr2 = [1, 2, 3, 4, 5]
+// console.log(arr2);
+// var obj = {
+//     name: 'Talgat',
+//     age: 18,
+//     arr: [1, 2, 3, 4 ],
+//     arr2: arr2,
+//     boolean: false,
+//     obj2: {
+//         name: 'new'
+//     }
+// }
+// console.log(obj)
+
